6b7260a99143a980b75118aff861b5a5ef6a75c1
ohyama4z <45777685+ohyama4z@users.noreply.github.com>
2020-04-27 23:56:37 +0900

    HelloWorldぺこ！

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,12 @@
-const functions = require('firebase-functions');
+'use strict'
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const functions = require('firebase-functions')
+const {dialogflow} = require('actions-on-google')
+
+const app =dialogflow()
+
+app.intent('HelloWorld', (conv) => {
+    conv.close('ハローワールドぺこ！')
+})
+
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app)
