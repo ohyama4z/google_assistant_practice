9f33d567f4bcdae13426c40330e77b53cf776d3e
ohyama4z <45777685+ohyama4z@users.noreply.github.com>
2020-04-28 16:31:08 +0900

    Default Welcome Intentで挨拶がてら日付を答える

--- functions/index.js
+++ functions/index.js
@@ -2,8 +2,20 @@
 
 const functions = require('firebase-functions')
 const {dialogflow} = require('actions-on-google')
+require('date-utils')
 
 const app =dialogflow()
+
+app.intent('Default Welcome Intent', (conv) => {
+    conv.contexts.set('welcome', 0)
+    conv.contexts.set('helloworld', 3)
+
+    let date = new Date()
+    let dayOfWeek = ['日', '月', '火', '水', '木', '金', '土'][date.getDay()] + '曜日'
+    let today = date.toFormat('YYYY年M月D日')
+
+    conv.ask(`ハローワールド!\n今日は${today}、${dayOfWeek}です!`)
+})
 
 app.intent('HelloWorld', (conv) => {
     conv.close('ハローワールドぺこ！')
