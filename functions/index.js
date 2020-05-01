8dc393ac01e9a323b51dda47b95bc196cc88ce37
ohyama4z <45777685+ohyama4z@users.noreply.github.com>
2020-05-01 18:44:26 +0900

    できたぺこ！！！！！！！！！！！！！！！！！！！！！

--- functions/index.js
+++ functions/index.js
@@ -6,19 +6,24 @@ require('date-utils')
 
 const app =dialogflow()
 
-app.intent('Default Welcome Intent', (conv) => {
-    conv.contexts.set('welcome', 0)
-    conv.contexts.set('helloworld', 3)
+//各intentへの入力はconv.body.queryResult.queryTextで取得できる
 
+app.intent('Default Welcome Intent', conv => {
     let date = new Date()
     let dayOfWeek = ['日', '月', '火', '水', '木', '金', '土'][date.getDay()] + '曜日'
     let today = date.toFormat('YYYY年M月D日')
 
-    conv.ask(`ハローワールド!\n今日は${today}、${dayOfWeek}です!`)
+    conv.ask(`こんぺこ～\n今日は${today}、${dayOfWeek}ぺこよ！`)
 })
 
-app.intent('HelloWorld', (conv) => {
-    conv.close('ハローワールドぺこ！')
+app.intent('HelloWorld', conv => {
+    console.log(conv.body.queryResult.queryText)
+    conv.ask('ハローワールドぺこ！')
+})
+
+app.intent('Peco', conv => {
+    const tell = conv.body.queryResult.queryText + 'ぺこ！'
+    conv.ask(tell)
 })
 
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app)
